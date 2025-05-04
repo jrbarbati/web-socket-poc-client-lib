e07fbb68351217db83b4c234fe214e9a55136e83
= <=>
2025-05-04 18:36:44 -0400

    updating javadoc on the @distributed batch annotation

--- src/main/java/com/personal/batch/hatcher/annotation/DistributedBatch.java
+++ src/main/java/com/personal/batch/hatcher/annotation/DistributedBatch.java
@@ -6,65 +6,67 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * If no orgId can be specified on the annotation, then you can use {@code @OrgIdParam} on the method this annotates to
- * pass the orgId into the request for the distributed batch. orgId not required for the distributed batch.
+ * If no {@code orgId} can be specified on the annotation, then you can use {@code @OrgIdParam} on the method this annotates to
+ * pass the {@code orgId} into the request for the distributed batch. {@code orgId} not required for the distributed batch.
  * Examples:
- *
- * @DistributedBatch(name = "genericScheduledProcess", orgId = 2)
+ * <pre class="code">
+ * &#064;DistributedBatch(name = "genericScheduledProcess", orgId = 2)
  * public void runForOrgId()
  * {
  *     ...
  * }
  *
  *
- * @DistributedBatch(name = "genericScheduledProcess")
- * protected void runForOrgId(@OrgIdParam int orgId, ...)
+ * &#064;DistributedBatch(name = "genericScheduledProcess")
+ * protected void runForOrgId(&#064;OrgIdParam int orgId, ...)
  * {
  *     ...
  * }
  *
- * @DistributedBatch(name = "genericScheduledProcess")
+ * &#064;DistributedBatch(name = "genericScheduledProcess")
  * protected void runForOrgId(...)
  * {
  *     // No orgId so the distributed batch will request to run without an orgId specified.
  *     ...
  * }
- *
- * It would seem the @DistributedBatch implementation will require abstracting the actual running of the batch job to another
- * class/component. This is due to the fact that Spring will not run the Aspect code for the @DistributedBatch annotation
+ * </pre>
+ * <p>
+ * It would seem the {@code @DistributedBatch} implementation will require abstracting the actual running of the batch job to another
+ * class/component. This is due to the fact that Spring will not run the Aspect code for the {@code @DistributedBatch} annotation
  * if it is called within the same class.
  * For example:
- *
- * @Component
+ * <pre class="code">
+ * &#064;Component
  * public class ScheduledComponent
  * {
- *     @Autowired
+ *     &#064;Autowired
  *     AbstractedExecutor abstractedExecutor;
  *
- *     @Scheduled(cron="...")
+ *     &#064;Scheduled(cron="...")
  *     public void do()
  *     {
  *         abstractedExecutor.doIt(...);
  *     }
  * }
  *
- * @Component
+ * &#064;Component
  * public class AbstractedExecutor
  * {
- *     @DistributedBatch(name = "doIt")
- *     public void doIt(@OrgIdParam int orgId, ...)
+ *     &#064;DistributedBatch(name = "doIt")
+ *     public void doIt(&#064;OrgIdParam int orgId, ...)
  *     {
  *         ...
  *     }
  *
  *     // OR
- *     @DistributedBatch(name = "doIt")
+ *
+ *     &#064;DistributedBatch(name = "doIt")
  *     public void doIt(...)
  *     {
  *         ...
  *     }
- *
  * }
+ * </pre>
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
