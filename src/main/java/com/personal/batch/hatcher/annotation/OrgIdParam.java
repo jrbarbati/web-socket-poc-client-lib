f1ee5e2d084e54f07c7b3a11f8bb6783ab9e9da4
= <=>
2025-05-04 18:39:25 -0400

    updating the java doc for @OrgIdParam

--- src/main/java/com/personal/batch/hatcher/annotation/OrgIdParam.java
+++ src/main/java/com/personal/batch/hatcher/annotation/OrgIdParam.java
@@ -7,8 +7,12 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * {@code @OrgIdParam} should mark the param in the method that is the orgId, if one exists.
+ * Used in conjunction with {@code @DistributedBatch}.
+ *
+ * {@code @OrgIdParam} should mark the param in the method that is the {@code orgId}, if one exists.
  * The param MUST be of type int or Integer.
+ *
+ * See {@link com.personal.batch.hatcher.annotation.DistributedBatch @DistributedBatch}
  */
 @Target(ElementType.PARAMETER)
 @Retention(RetentionPolicy.RUNTIME)
